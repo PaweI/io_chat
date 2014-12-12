e1a1d6b73cf1b9e8690ed3eebd25f34e4797072e
Pavel Redics <pavel.redics@gmail.com>
2014-12-12 11:56:52 +0000

    change port to 3000

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ io.on('connection', function(socket){
   });
 });
 
-server.listen(process.env.PORT || 5000, function() {
+server.listen(process.env.PORT || 3000, function() {
   console.log('listening on port:3000');
 });
